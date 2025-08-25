8dd7ecd1a0e3c034ee8c89fcec22d1c3d7424030
getmeds43-ui <getmeds43@gmail.com>
2025-08-25 10:05:20 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("hello");
+
+
+git config --global user.name ""
+git config --global user.email ""
+
+
+git init
+git status
+git branch
+git add .
+git commit -m "message"
+
+git remote add origin link
+git push -u origin branch_name
+
+--------------------
+git add .
+git commit -m "update"
+git push -u origin branch_name
